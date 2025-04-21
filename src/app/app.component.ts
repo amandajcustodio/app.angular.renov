bc1ac0c70c6fa1df5063e0ac5ef4e0aa82855686
Amanda Custodio <amandajcustodio@outlook.com>
2025-04-20 22:16:44 -0300

    feat(init): initialized project

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
+
+@Component({
+  selector: 'app-root',
+  imports: [ButtonModule],
+  template: ``,
+
+})
+export class AppComponent {
+  title = 'app.angular.renov';
+}
